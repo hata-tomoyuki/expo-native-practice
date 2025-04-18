83a870ee3fc4ba73bfe4ad63a5429ca7d01f0091
hata-tomoyuki <h.tomo0602@gmail.com>
2025-04-19 06:47:04 +0900

    詳細ページでのデータフェッチ

--- app/page2/[id].tsx
+++ app/page2/[id].tsx
@@ -1,14 +1,48 @@
 import { useLocalSearchParams } from 'expo-router'
+import { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 
-export default function TodoDetail() {
-  const { id } = useLocalSearchParams()
+type Todo = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
 
-  return (
-    <View style={{ padding: 20 }}>
-      <Text style={{ fontSize: 24 }}>Todoの詳細ページ</Text>
-      <Text>ID: {id}</Text>
-      {/* 必要ならここでfetchして詳細情報を取得してもOK */}
-    </View>
-  )
+
+export default function TodoDetail() {
+    const { id } = useLocalSearchParams()
+    const [todo, setTodo] = useState<Todo>({
+        userId: 0,
+        id: 0,
+        title: '',
+        completed: false,
+    })
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        console.log('Todo ID:', id)
+        fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                setIsLoading(false)
+                setTodo(json)
+            })
+    }, [])
+
+    return (
+        <View style={{ padding: 20 }}>
+            <Text style={{ fontSize: 24 }}>Todoの詳細ページ</Text>
+            {isLoading ? <Text>Loading...</Text> : (
+                <View>
+                    <Text>ID: {id}</Text>
+                    <Text>タイトル: {todo.title}</Text>
+                    <Text>完了: {todo.completed ? '完了' : '未完了'}</Text>
+                    <Text>ユーザーID: {todo.userId}</Text>
+                    <Text>Todo ID: {todo.id}</Text>
+                </View>
+            )}
+        </View>
+    )
 }
