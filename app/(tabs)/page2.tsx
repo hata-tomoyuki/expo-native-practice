5b76d2ee0ce863747848f4368b2f20f4cf3179eb
hata-tomoyuki <h.tomo0602@gmail.com>
2025-04-19 06:24:53 +0900

    FlatList

--- app/(tabs)/page2.tsx
+++ app/(tabs)/page2.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 type Todo = {
@@ -10,12 +10,15 @@ type Todo = {
 
 const page2 = () => {
     const [todos, setTodos] = useState<Todo[]>([])
+    const [isLoadng, setIsLoading] = useState(true)
 
     useEffect(() => {
+        setIsLoading(true)
         fetch('https://jsonplaceholder.typicode.com/todos')
             .then(response => response.json())
             .then(json => {
                 console.log(json)
+                setIsLoading(false)
                 setTodos(json)
             })
     }, [])
@@ -24,14 +27,25 @@ const page2 = () => {
         <View>
             <Text>page2</Text>
             <Text>todos</Text>
-            <ScrollView>
+            {isLoadng && <Text>Loading...</Text>}
+            {/* <ScrollView>
                 {todos.map((todo) => (
                     <View key={todo.id}>
                         <Text>{todo.title}</Text>
                         <Text>{todo.completed ? '完了' : '未完了'}</Text>
                     </View>
                 ))}
-            </ScrollView>
+            </ScrollView> */}
+            <FlatList
+                data={todos}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item.title}</Text>
+                        <Text>{item.completed ? '完了' : '未完了'}</Text>
+                    </View>
+                )}
+                />
         </View>
     )
 }
