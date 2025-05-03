5e88849e1dd269fd006b246f821c11e0ea7205da
hata-tomoyuki <h.tomo0602@gmail.com>
2025-05-03 11:02:36 +0900

    ページ遷移

--- app/data/[id].tsx
+++ app/data/[id].tsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, Stack, useRouter } from 'expo-router'
 
 const DataDetailPage = () => {
     const { userId, id, title, completed } = useLocalSearchParams()
+    const router = useRouter()
 
     return (
-        <View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Stack.Screen
+                options={{
+                    title: '詳細',
+                    headerBackTitle: '戻る',
+                }}
+            />
             <Text>{title}</Text>
-            <Text>{completed ? '完了' : '未完了'}</Text>
+            <Text>{completed ? '完了' : '未完了'}!!!!!!!!!!!</Text>
+
+            <TouchableOpacity
+                onPress={() => {
+                    router.back()
+                }}
+            >
+                <Text>戻る</Text>
+            </TouchableOpacity>
         </View>
     )
 }
